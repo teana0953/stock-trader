6c1f8578497974bc20faef202cb718848c5ed520
Tina.Liang <teana0953@gmail.com>
2020-10-05 23:10:19 +0800

    Update Header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+    <b-container>
+        <app-header />
+        <b-row>
+            <b-col xs="12">
+                <router-view />
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
+<script>
+import Header from './components/Header.vue';
+export default {
+    components: {
+        appHeader: Header,
+    },
+};
+</script>
+
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-#nav {
+body {
     padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="secondary">
+        <b-navbar-brand to="/">Stock Trader</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item to="/portfolio">Portfolio</b-nav-item>
+                <b-nav-item to="/stocks">Stocks</b-nav-item>
+            </b-navbar-nav>
+
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item right>End Day</b-nav-item>
+                <b-nav-item-dropdown right>
+                    <!-- Using 'button-content' slot -->
+                    <template v-slot:button-content>
+                        Save & Load
+                    </template>
+                    <b-dropdown-item href="#">Save Data</b-dropdown-item>
+                    <b-dropdown-item href="#">Load Data</b-dropdown-item>
+                </b-nav-item-dropdown>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
